e937b82c930a7e3ddc5aaf5741c05f2178de7f42
Omer <teouysal@outlook.com>
2021-10-14 17:57:08 +0300

    created project

---
+++ instagram_clone/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import HomeScreen from '../src/screens/HomeScreen';
+
+const App = () => {
+  return <HomeScreen />;
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
