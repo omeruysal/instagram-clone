331d22776d7d6f5ff03c6f3380216bdb86cd617e
Omer <teouysal@outlook.com>
2021-10-14 17:58:42 +0300

    changed folder structure

--- instagram_clone/App.js
+++ instagram_clone/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
-import HomeScreen from '../src/screens/HomeScreen';
+import HomeScreen from './src/screens/HomeScreen';
 
 const App = () => {
   return <HomeScreen />;
